a9da42ba08268098cfc6a94896f67a70bb59b9f0
Brianna Lewis <122322750+blewis722@users.noreply.github.com>
2023-05-13 00:05:34 -0400

    Add files via upload

--- itis3135/project_1/scripts/projectscript.js
+++ itis3135/project_1/scripts/projectscript.js
@@ -13,8 +13,8 @@ $(function() {
               "alt": "About Page" 
           },
           "3": {
-              "url": "experience.html",
-              "text": "Experience",
+              "url": "services.html",
+              "text": "Services",
               "alt": "Exoerience Page"
           },
           "4": {
@@ -23,7 +23,7 @@ $(function() {
              "alt": "Portfolio Page"
            },
           "5": {
-            "url": "Contact.html",
+            "url": "contact.html",
             "text": "Contact",
             "alt": "Contact Page"
           }
@@ -47,3 +47,74 @@ $(function() {
  
  
   });
+
+const zoomBtn = document.querySelectorAll('.zoom-text');
+const allImages = document.querySelectorAll('.img-container');
+const imageView = document.querySelector('.image-view') ||  document.querySelector('.image-view2') || document.querySelector('.image-view3');
+
+const nextBtn = document.getElementById('next-btn');
+const prevBtn =  document.getElementById('prev-btn');
+const imageBox = document.querySelector('.image-box') || document.querySelector('.image-box2') || document.querySelector('.image-box3');
+
+let currentImageIdx = 0;
+imageView.addEventListener('click', function(){
+  this.style.display = "none";
+  imageBox.style.display = "none";
+});
+
+zoomBtn.forEach(function(btn, index){
+  btn.addEventListener('click',function(){
+    imageView.style.display ="block";
+    imageBox.style.display = "block";
+    currentImageIdx = index +1;
+    currentImageDisplay( currentImageIdx );
+  });
+});
+
+function currentImageDisplay(position){
+if(imageView === document.querySelector('.image-view') && (imageBox === document.querySelector('.image-box'))){
+  return imageBox.style.background = `url(images/weddingImg${currentImageIdx}.jpg) center/cover no-repeat`;
+}else {
+  if(imageView === document.querySelector('.image-view2')){
+ 
+  return imageBox.style.background = `url(images/engageImg${currentImageIdx}.jpg) center/cover no-repeat`;
+}
+
+
+} 
+if(imageView === document.querySelector('.image-view3')){
+  return imageBox.style.background = `url(images/anniversaryImg${currentImageIdx}.jpg) center/cover no-repeat`;
+}
+ 
+}
+
+
+prevBtn.addEventListener('click', function(){
+  currentImageIdx--;
+  if(currentImageIdx === 0){
+    currentImageIdx = allImages.length;
+  }
+  currentImageDisplay(currentImageIdx);
+ 
+});
+
+nextBtn.addEventListener('click', function(){
+  currentImageIdx++;
+  if(currentImageIdx === 13){
+    currentImageIdx = 1;
+  }
+  currentImageDisplay(currentImageIdx);
+  
+});
+
+
+let formModal = document.getElementById('id01');
+
+window.onclick = function(event){
+  if(event.target == modal){
+    formModal.style.display = "none";
+  }
+}
+
+
+
